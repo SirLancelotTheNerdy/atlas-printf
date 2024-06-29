6191688f97347a0d47ffa9b1595bd8c517998578
SirLancelotTheNerdy <lancelot.gay@atlasschool.com>
2024-06-29 17:31:45 -0500

    Started portotype in get_

--- get_printf.c
+++ get_printf.c
@@ -1,3 +1,13 @@
-#include main.h 
+#include "main.h" 
 
 /* this is where we loop through the protatypes fro the main printf.c */
+
+int _printf(const char *format, ...)
+{
+	int char_print = 0;
+	va_list list_of_args;
+
+	if(format == NULL)
+		return -1;
+	va_start(list_of_args, format);
+	while(*format)
