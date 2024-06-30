ba97da3d047329a17943670ecfd2d835f13c7b0e
SirLancelotTheNerdy <lancelot.gay@atlasschool.com>
2024-06-29 19:27:43 -0500

    added my ides for the main fuction it get_ and added my notes of how it works in the man_ page.

--- get_printf.c
+++ get_printf.c
@@ -5,9 +5,63 @@
 int _printf(const char *format, ...)
 {
 	int char_print = 0;
-	va_list list_of_args;
+	va_list args;
 
 	if(format == NULL)
 		return -1;
-	va_start(list_of_args, format);
-	while(*format)
+	va_start(args, format);
+	while(args)
+{
+		if(args != '%')
+		{
+			write(1, args, 1);
+			char_print++;
+		}
+		else
+		{
+			args++;
+			if(args == '\0')
+			{
+				return (chara_print);
+			}
+			else if(args == 'c')
+			{
+				char c = va_arg(*formant, int);
+				write(1, &c, 1);
+				chara_print++;
+			}
+			else if(args == 's')
+			{
+				char *str = va_arg(*formant, char*);
+				int str_len = 0;
+				if(str == NULL)
+					str = "(null)";
+				while (str[str_len] != '\0')
+					str_len++;
+				write(1, str, str_len);
+				chara_print += str_len;
+			}
+			else if(args == '%')
+				{
+					if (args + 1) == '%')
+					{
+						return (0);
+					}
+					else
+					{
+						write(1, args, 1);
+						chara_print++;
+					}
+				}
+			else if (args == 'd' || args == 'i')
+			{
+			    
+			chara_print += print_num(args);
+			write(1, &chara_print, 4);
+			}
+		}
+		args++;
+	}
+	va_end(args);
+	va_end(*formant);
+	return chara_print;
