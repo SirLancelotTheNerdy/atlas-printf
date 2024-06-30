e133fc148a7fa038be5388f73841e72029013fc0
PatrickMckay1998 <168458082+PatrickMckay1998@users.noreply.github.com>
2024-06-29 19:35:50 -0500

    Programmed printf.c, created a manual for my program

--- get_printf.c
+++ get_printf.c
@@ -1,3 +1,2 @@
 #include main.h 
 
-/* this is where we loop through the protatypes fro the main printf.c */
--- printf.c
+++ printf.c
@@ -1,1 +1,48 @@
-/*will refrence the outcome of get_ to displsy printf*/
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+    const char *ptr;
+
+    va_start(args, format);
+    for (ptr = format; *ptr != '\0'; ptr++)
+    {
+        if (*ptr == '%')
+        {   
+            char c = (char) va_arg(args, int);
+            write(1, &c, 1);
+            count++;
+        }
+        else if (*ptr == 's')
+        {
+            char *s = va_arg(args, char*);
+            while (*s)
+            {
+                write(1, s++, 1);
+                count++;
+            }
+        }
+        else if (*ptr == '%') 
+        {
+            char c = '%';
+            write(1, &c, 1);
+            count++;
+        }
+        else 
+        {
+            write(1, ptr - 1, 1);
+            write(1, ptr, 1);
+            count += 2;
+        }
+        else 
+        {
+            write(1, ptr, 1);
+            count++;
+        }
+    }
+    va_end(args);
+
+    return (count);
+}
